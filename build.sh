94a20beabed49a019a212fb545c213f4baac658c
break <breakdevx@gmail.com>
2024-09-29 15:28:08 +0900

    gitignore

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-GOOS=linux GOARCH=amd64 go build -o ./bin/redi301_$(date +%Y%m%d_%H%M%S)
+file_name=$(date +%Y%m%d_%H%M%S)
+GOOS=linux GOARCH=amd64 go build -o ./bin/redi301_"$file_name"
+cp -f ./bin/redi301_"$file_name" ./bin/redi301
